96c2476ddcdff8e2537fa795605340f968998141
Murodjon01 <boboqulovm111@gmail.com>
2023-03-29 20:18:13 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# # Press Shift+F10 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#
+# class A():
+#     def disp(self):
+#         print("A disp()")
+# class B():
+#     pass
+#
+# obj = B()
+# obj.disp()
+# print("A disp()")
+employee (person_name, street, city)
+works (person_name, company name, salary)
+company (company name, city)
+
+Find the name of each employee whose salary is greater than $100000.?
